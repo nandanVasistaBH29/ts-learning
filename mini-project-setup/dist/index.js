300326a07793b99a332da61f6064526e6725ae77
nandan vasista <nandan@nandan.local>
2023-02-07 22:12:01 +0530

    TS classes

--- mini-project-setup/dist/index.js
+++ mini-project-setup/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("TS is running in Watch mode");
+//classes in TS is similar to that of JAVA but one diff is the constructor keyword is used for constructor not the class name
+// but
+// You can't use multiple constructors, but you can add a few optional parameters and verify if it exists
+class User {
+  constructor(name, email, city = "") {
+    this.name = name;
+    this.email = email;
+    this.city = city;
+  }
+}
--- mini-project-setup/src/index.ts
+++ mini-project-setup/src/index.ts
@@ -1,1 +1,13 @@
-console.log("TS is running in Watch mode");
+//classes in TS is similar to that of JAVA but one diff is the constructor keyword is used for constructor not the class name
+// but
+// You can't use multiple constructors, but you can add a few optional parameters and verify if it exists
+class User {
+  name: string;
+  email: string;
+  city: string;
+  constructor(name: string, email: string, city: string = "") {
+    this.name = name;
+    this.email = email;
+    this.city = city;
+  }
+}
