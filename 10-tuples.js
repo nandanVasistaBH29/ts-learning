704331a48f7d8a570c7c24764cc933dbd586fd73
nandan vasista <nandan@nandan.local>
2023-02-07 19:48:44 +0530

    tuples in TS are't strict enough

---
+++ 10-tuples.js
@@ -0,0 +1,16 @@
+"use strict";
+// https://stackoverflow.com/questions/64069552/typescript-array-push-method-cant-catch-a-tuple-type-of-the-array
+// are typles in TS not immutable
+exports.__esModule = true;
+// tuples in TS are like arrays with fixed length and fixed datatype for that position
+var rgb = [255, 255, 255];
+rgb[0] = 0;
+console.log(rgb); // u can change it
+// const user: [number, string]=['23213',122]/ order matters
+var user = [123, "nandan"];
+console.log(user);
+//problem
+// if the array has fixed schema u shouldn't be able to change it
+console.log("pop");
+user.pop();
+console.log(user);
---
+++ 10-tuples.ts
@@ -0,0 +1,18 @@
+// https://stackoverflow.com/questions/64069552/typescript-array-push-method-cant-catch-a-tuple-type-of-the-array
+// are typles in TS not immutable
+
+// tuples in TS are like arrays with fixed length and fixed datatype for that position
+
+const rgb: [number, number, number] = [255, 255, 255];
+rgb[0] = 0;
+console.log(rgb); // u can change it
+// const user: [number, string]=['23213',122]/ order matters
+const user: [number, string] = [123, "nandan"];
+console.log(user);
+
+//problem
+// if the array has fixed schema u shouldn't be able to change it
+console.log("pop");
+user.pop();
+console.log(user);
+export {};
