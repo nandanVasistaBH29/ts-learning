7ccce38265b56951b4b3a9774048b6b641f86bc1
nandan vasista <nandan@nandan.local>
2023-02-07 17:56:48 +0530

    objects

---
+++ 05-objects.js
@@ -0,0 +1,14 @@
+// to login u need email and password
+// the function is expected to return jwt_token and success or failure
+// const func_name = ({obj param}):{returning obj}=>{
+//body
+// }
+function login(_a) {
+    var email = _a.email, password = _a.password;
+    console.log(email, password);
+    return {
+        jwt_token: "1231231231231312",
+        isSuccess: true
+    };
+}
+console.log(login({ email: "nandan@gmail.com", password: "*******" }));
---
+++ 05-objects.ts
@@ -0,0 +1,18 @@
+// to login u need email and password
+// the function is expected to return jwt_token and success or failure
+
+// const func_name = ({obj param}):{returning obj}=>{
+//body
+// }
+
+function login({ email, password }): {
+  jwt_token: string;
+  isSuccess: boolean;
+} {
+  console.log(email, password);
+  return {
+    jwt_token: "1231231231231312",
+    isSuccess: true,
+  };
+}
+console.log(login({ email: "nandan@gmail.com", password: "*******" }));
